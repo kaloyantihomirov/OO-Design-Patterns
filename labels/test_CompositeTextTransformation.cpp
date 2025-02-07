52c4c83b479342d1be913aa00d8f57d36fa42cb3
Kaloyan Kolev <kaloyan_tihomirov@icloud.com>
2025-02-07 02:18:32 +0200

    finished testing composite transformation

--- labels/test_CompositeTextTransformation.cpp
+++ labels/test_CompositeTextTransformation.cpp
@@ -5,6 +5,20 @@
 
 using namespace fakeit;
 
+//visible only in current translation unit (can be used as alternative of FakeIt which I used
+//as a mocking framework)
+namespace
+{
+    class DummyTransformation : public TextTransformation
+    {
+    public:
+        std::string transform(const std::string& text) const override
+        {
+            return text;
+        }
+    };
+}
+
 TEST_CASE("CompositeTransformation calls each transformation in sequence")
 {
     Mock<TextTransformation> mock1;
@@ -39,6 +53,42 @@ TEST_CASE("CompositeTransformation calls each transformation in sequence")
     Verify(Method(mock2, transform)).Exactly(2);
 }
 
+TEST_CASE("CompositeTransformation applies transformations in correct order when 3 transformations available")
+{
+    Mock<TextTransformation> mock1;
+    When(Method(mock1, transform)).AlwaysDo([](const std::string& text) {
+        return text + "1";
+        });
+
+    Mock<TextTransformation> mock2;
+    When(Method(mock2, transform)).AlwaysDo([](const std::string& text) {
+        return text + "2";
+        });
+
+    Mock<TextTransformation> mock3;
+    When(Method(mock3, transform)).AlwaysDo([](const std::string& text) {
+        return text + "3";
+        });
+
+    auto sp1 = std::shared_ptr<TextTransformation>(&mock1.get(), [](TextTransformation*) {});
+    auto sp2 = std::shared_ptr<TextTransformation>(&mock2.get(), [](TextTransformation*) {});
+    auto sp3 = std::shared_ptr<TextTransformation>(&mock3.get(), [](TextTransformation*) {});
+
+    std::vector<std::shared_ptr<TextTransformation>> transformations;
+    transformations.push_back(sp2);
+    transformations.push_back(sp1);
+	transformations.push_back(sp3);
+
+    CompositeTransformation composite(transformations);
+
+    std::string input = "Test";
+    REQUIRE(composite.transform(input) == "Test213");
+
+    Verify(Method(mock1, transform)).Once();
+    Verify(Method(mock2, transform)).Once();
+    Verify(Method(mock3, transform)).Once();
+}
+
 TEST_CASE("CompositeTransformation returns input unchanged with no transformations")
 {
     std::vector<std::shared_ptr<TextTransformation>> emptyTransformations;
@@ -69,5 +119,29 @@ TEST_CASE("CompositeTransformation applies a single transformation correctly")
     Verify(Method(mock1, transform)).Once();
 }
 
+TEST_CASE("CompositeTransformation throws exception if vector contains nullptr")
+{
+    std::vector<std::shared_ptr<TextTransformation>> transformations;
 
+    SECTION("First element is nullptr")
+    {
+        transformations.push_back(nullptr);
+        REQUIRE_THROWS_AS(CompositeTransformation(transformations), std::invalid_argument);
+    }
+    SECTION("Other element is nullptr")
+    {
+        auto j = GENERATE(15, 40, 98);
+
+        DYNAMIC_SECTION(j << "th" << "element is nullptr (0 indexed)")
+        {
+            for (int i = 0; i < j; i++)
+            {
+                transformations.push_back(std::make_shared<DummyTransformation>());
+            }
+            transformations.push_back(nullptr);
+
+            REQUIRE_THROWS_AS(CompositeTransformation(transformations), std::invalid_argument);
+        }
+    }
+}
 
